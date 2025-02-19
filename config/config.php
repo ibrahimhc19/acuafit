efe158f4799239dce9a7bb9852d32810a300ed94
Ibrahim Calzadilla <112896867+ibrahimhc19@users.noreply.github.com>
2025-02-19 16:39:57 -0500

    config

--- config/config.php
+++ config/config.php
@@ -1,13 +1,27 @@
 <?php
 
-
 define("SERVIDOR", "localhost");
 define("USUARIO", "root");
 define("PASSWORD", "");
-define("BD", "acuafit");
+define("DB", "acuafit");
 
 define("APP_NAME", "ADMINISTRACIÓN ACUAFIT");
 define("APP_URL", "http://localhost/acuafit");
 define("KEY_API_MAPS", "");
 
-$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+$servidor = "mysql:dbname=" . DB . ";host=" . SERVIDOR;
+
+try {
+    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    echo "Conexión exitosa a la base de datos";
+} catch (PDOException $e) {
+    echo "Error, no se pudo conectar a la base de datos: " . $e->getMessage();
+}
+
+date_default_timezone_set("America/Bogota");
+$fechaHora = date("Y-m-d H:i:s");
+$fecha_ctual = date("Y-m-d");
+$dia_actual = date("d");
+$mes_actual = date("m");
+$anno_domini = date("Y");
+
