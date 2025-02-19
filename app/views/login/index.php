328c9d0f84949ef030a9b5dd610ab838315cd279
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2025-02-18 22:53:07 -0500

    first commit

---
+++ app/views/login/index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Administración Acuafit</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../../public/css/styles.css">
+
+</head>
+
+<body>
+
+<div class="container">
+    <div class="row form">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card border-0 shadow rounded-3 my-5">
+          <div class="card-body p-4 p-sm-5">
+            <img src="../../../public/images/logo.png" alt="" width="120px">
+            <h5 class="card-title text-center mb-4 fw-light fs-5">Inicio de Sesión</h5>
+
+            <!-- action="inicio.php" method="post" -->
+            <form>
+              <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Usuario</label>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Contraseña</label>
+              </div>
+
+              <div class="form-check mb-3">
+                <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
+                <label class="form-check-label" for="rememberPasswordCheck">
+                  Recordar Credenciales
+                </label>
+              </div>
+              <div class="d-grid">
+                <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Iniciar sesión</button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+define("SERVIDOR", "localhost");
+define("USUARIO", "root");
+define("PASSWORD", "");
+define("BD", "acuafit");
+
+define("APP_NAME", "ADMINISTRACIÓN ACUAFIT");
+define("APP_URL", "http://localhost/acuafit");
+define("KEY_API_MAPS", "");
+
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
