16c61b2c7c5d94b213a5de13ec2d95938eb27e19
Ibrahim Calzadilla <112896867+ibrahimhc19@users.noreply.github.com>
2025-02-19 17:04:29 -0500

    usando base url

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,16 +1,19 @@
 
+<?php
 
+include("app/config.php");
 
+>
 
 <!doctype html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Administración Acuafit</title>
+  <title><?=APP_NAME?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="../../../public/css/styles.css">
+  <link rel="stylesheet" href="<?=APP_URL?>public/css/styles.css">
 
 </head>
 
@@ -21,7 +24,7 @@
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card border-0 shadow rounded-3 my-5">
           <div class="card-body p-4 p-sm-5">
-            <img src="../../../public/images/logo.png" alt="" width="120px">
+            <img src="<?=APP_URL?>public/images/logo.png" alt="" width="120px">
             <h5 class="card-title text-center mb-4 fw-light fs-5">Inicio de Sesión</h5>
 
             <!-- action="inicio.php" method="post" -->
